fix weight update and gradient

modules/gradient.py
@@ -1,154 +1,168 @@
 import numpy as np
-from modules.smc import Hamiltonian
 from modules.hamiltonian import Hamiltonian
 
 
-def conjugated_angles(original_angles):
-    """
-    Here we create new series of angles for gradient measurements. We use here the notation of bloch sphere basis.
-    2 letters notation is exhaustively because basis choice is repeated for the following spines.
-    For example "YZ" means "YZYZYZ...". Bloch notation means:
-        - Z: theta=theta, phi=phi
-        - X: theta=theta+pi/2, phi=phi
-        - Y: thetha=theta+pi/2, phi=phi+pi/2
+class ProbabilityDerivative:
+    """Perform measurements and calculates derivatives of probabilites for hamiltonian"""
+    def __init__(self, hamiltonian: Hamiltonian, original_angles):
+        self.hamiltonian = hamiltonian
+        self.original_angles = original_angles
+        self.n_spins = self.hamiltonian.n_spins
 
-    """
-    n_spins = len(original_angles)
+    def conjugated_angles(self):
+        """
+        Here we create new series of angles for gradient measurements. We use here the notation of bloch sphere basis.
+        2 letters notation is exhaustively because basis choice is repeated for the following spines.
+        For example "YZ" means "YZYZYZ...". Bloch notation means:
+            - Z: theta=theta, phi=phi
+            - X: theta=theta+pi/2, phi=phi
+            - Y: thetha=theta+pi/2, phi=phi+pi/2
 
-    ZZ = np.copy(original_angles)
-    XZ = np.copy(original_angles)
-    YZ = np.copy(original_angles)
-    ZX = np.copy(original_angles)
-    ZY = np.copy(original_angles)
-    for i in range(n_spins):
-        if i % 2 == 0:
-            XZ[i, 0] += np.pi / 2
-            YZ[i] += np.pi / 2  # Both angles are increases by pi/2
-        else:
-            ZX[i, 0] += np.pi / 2
-            ZY[i] += np.pi / 2  # Both angles are increases by pi/2
-    return ZZ, XZ, YZ, ZX, ZY
+        """
+        original_angles = self.original_angles
 
+        ZZ = np.copy(original_angles)
+        XZ = np.copy(original_angles)
+        YZ = np.copy(original_angles)
+        ZX = np.copy(original_angles)
+        ZY = np.copy(original_angles)
+        for i in range(self.n_spins):
+            if i % 2 == 0:
+                XZ[i, 0] += np.pi / 2
+                YZ[i] += np.pi / 2  # Both angles are increases by pi/2
+            else:
+                ZX[i, 0] += np.pi / 2
+                ZY[i] += np.pi / 2  # Both angles are increases by pi/2
+        return ZZ, XZ, YZ, ZX, ZY
 
-def measurements_for_gradient(hamiltonian: Hamiltonian, original_angles):
-    # TODO: make correlators
-    """
+    def measurements_for_gradient(self):
+        # TODO: make correlators
+        """
+        It's a key function of our work, where measurements are performed.
+        There are 2 types of basis choices:
+            - Original (O) with theta, phi from our primary basis choice. Label it as "/"
+            - Conjugated (C) with theta + pi/2, phi. Label it as "\"
+        We perform 3 types of measurement:
+            - All original (orig). With labels it looks like : "//////" for 6 spins
+            - Even spins in conjugated basis and odd in original (even): (spins are numerated from 0)
+            - Even spins in original basis and odd in conjugated (odd) :
+        Then we take singles and correlators from this measurements. We name it like, for example:
+            - "singles_C" for singles in conjugated basis: \
+            - "correlators_OC" for correlators for first spin in original basis and second in conjugated: /\
+        """
+        n_spins = self.n_spins
+        hamiltonian = self.hamiltonian
+        # Generate angles for measurements
+        (ZZ, XZ, YZ, ZX, ZY) = self.conjugated_angles()
 
-    It's a key function of our work, where measurements are performed.
-    There are 2 types of basis choices:
-        - Original (O) with theta, phi from our primary basis choice. Label it as "/"
-        - Conjugated (C) with theta + pi/2, phi. Label it as "\"
-    We perform 3 types of measurement:
-        - All original (orig). With labels it looks like : "//////" for 6 spins
-        - Even spins in conjugated basis and odd in original (even): (spins are numerated from 0)
-        - Even spins in original basis and odd in conjugated (odd) :
-    Then we take singles and correlators from this measurements. We name it like, for example:
-        - "singles_C" for singles in conjugated basis: \
-        - "correlators_OC" for correlators for first spin in original basis and second in conjugated: /\
-    """
-    n_spins = hamiltonian.n_spins
+        # Perform measurements
+        # raw_singles_ZZ, raw_correlators_ZZ = hamiltonian.measure(ZZ)
+        # raw_singles_XZ, raw_correlators_XZ = hamiltonian.measure(XZ)
+        # raw_singles_YZ, raw_correlators_YZ = hamiltonian.measure(YZ)
+        # raw_singles_ZX, raw_correlators_ZX = hamiltonian.measure(ZX)
+        # raw_singles_ZY, raw_correlators_ZY = hamiltonian.measure(ZY)
+        raw_singles_ZZ = hamiltonian.measure(ZZ)
+        raw_singles_XZ = hamiltonian.measure(XZ)
+        raw_singles_YZ = hamiltonian.measure(YZ)
+        raw_singles_ZX = hamiltonian.measure(ZX)
+        raw_singles_ZY = hamiltonian.measure(ZY)
 
-    # Generate angles for measurements
-    (ZZ, XZ, YZ, ZX, ZY) = conjugated_angles(original_angles)
+        # In Z basis we immediately got answers, so just for consistency:
+        singles_Z = raw_singles_ZZ
+        # correlators_ZZ = raw_correlators_ZZ
 
-    # Perform measurements
-    raw_singles_ZZ, raw_correlators_ZZ = hamiltonian.measure(ZZ)
-    raw_singles_XZ, raw_correlators_XZ = hamiltonian.measure(XZ)
-    raw_singles_YZ, raw_correlators_YZ = hamiltonian.measure(YZ)
-    raw_singles_ZX, raw_correlators_ZX = hamiltonian.measure(ZX)
-    raw_singles_ZY, raw_correlators_ZY = hamiltonian.measure(ZY)
+        # Create blanks for other singles and correlators
+        singles_X = np.zeros((n_spins, 2))
+        singles_Y = np.zeros((n_spins, 2))
+        # correlators_XZ = np.zeros((n_spins - 1, 4))
+        # correlators_YZ = np.zeros((n_spins - 1, 4))
 
-    # In Z basis we immediately got answers, so just for consistency:
-    singles_Z = raw_singles_ZZ
-    correlators_ZZ = raw_correlators_ZZ
+        for i in range(n_spins):
+            if i % 2 == 0:
+                singles_X[i] = raw_singles_XZ[i]
+                singles_Y[i] = raw_singles_YZ[i]
+                # if i + 1 < n_spins:
+                #     correlators_XZ[i] = raw_correlators_XZ[i]
+                #     correlators_YZ[i] = raw_correlators_YZ[i]
+            else:
+                singles_X[i] = raw_singles_ZX[i]
+                singles_Y[i] = raw_singles_ZY[i]
+                # if i + 1 < n_spins:
+                #     correlators_XZ[i] = raw_correlators_ZX[i]
+                #     correlators_YZ[i] = raw_correlators_ZY[i]
 
-    # Create blanks for other singles and correlators
-    singles_X = np.zeros((n_spins, 2))
-    singles_Y = np.zeros((n_spins, 2))
-    correlators_XZ = np.zeros((n_spins - 1, 4))
-    correlators_YZ = np.zeros((n_spins - 1, 4))
+        self.singles_Z = singles_Z
+        self.singles_X = singles_X
+        self.singles_Y = singles_Y
 
-    for i in range(n_spins):
-        if i % 2 == 0:
-            singles_X[i] = raw_singles_XZ[i]
-            singles_Y[i] = raw_singles_YZ[i]
-            if i + 1 < n_spins:
-                correlators_XZ[i] = raw_correlators_XZ[i]
-                correlators_YZ[i] = raw_correlators_YZ[i]
-        else:
-            singles_X[i] = raw_singles_ZX[i]
-            singles_Y[i] = raw_singles_ZY[i]
-            if i + 1 < n_spins:
-                correlators_XZ[i] = raw_correlators_ZX[i]
-                correlators_YZ[i] = raw_correlators_ZY[i]
+    def get_ABC(self):
 
-    return singles_Z, singles_X, singles_Y
+        theta = self.original_angles[:, 0]  # we need only thetas
 
+        self.measurements_for_gradient()
 
-def single_gradient(coefs, angles):
-    """
-    By given coefficients [A, B, C] and angles [theta, phi] returns corresponding single derivative
-        A = 1/2 - rho_00
-        B = Re(rho_01 * e^{i*phi}
-        C = Im(rho_01 * e^{i*phi}
-    """
-    A, B, C = coefs
-    theta = angles[:, 0] # we need only thetas
+        # Probabilities of getting 0 in each basis
+        p_Z, p_X, p_Y = self.singles_Z[:, 0], self.singles_X[:, 0], self.singles_Y[:, 0]
 
-    d_theta = A * np.cos(theta) + B * np.sin(theta)
-    d_phi = - C * np.sin(theta)
+        # Auxiliary vectors
+        V1 = p_Z - ((np.sin(theta / 2)) ** 2) - (1 / 2) * np.cos(theta)
+        V2 = p_X - (1 / 2) * (np.sin(theta) + 1) + (1 / 2) * np.sin(theta)
 
-    d_angles = np.array([d_theta, d_phi]).T
+        A = - np.cos(theta) * V1 + np.sin(theta) * V2
+        B = np.sin(theta) * V1 + np.cos(theta) * V2
+        C = (1 / 2) - p_Y
 
-    return d_angles
+        self.coefs = np.array([A, B, C])
+        return self.coefs
 
+    def d_prob(self):
+        """
+        By given coefficients [A, B, C] and angles [theta, phi] returns corresponding single derivative
+            A = 1/2 - rho_00
+            B = Re(rho_01 * e^{i*phi}
+            C = Im(rho_01 * e^{i*phi}
+        """
+        A, B, C = self.get_ABC()
+        theta = self.original_angles[:, 0]  # we need only thetas
 
-def get_ABC(hamiltonian: Hamiltonian, angles):
+        # Actually, this is derivatives of probabilities by d_theta and d_phi
+        d_theta = A * np.sin(theta) + B * np.cos(theta)  # \frac{d_prob}{d_theta}
+        d_phi = - C * np.sin(theta)  # \frac{d_prob}{d_phi}
 
-    theta = angles[:, 0]  # we need only thetas
+        d_prob = np.array([d_theta, d_phi]).T
+        self.d_prob = d_prob
 
-    singles_Z, singles_X, singles_Y = measurements_for_gradient(hamiltonian, angles)
-    p_Z, p_X, p_Y = singles_Z[:, 0], singles_X[:, 0], singles_Y[:, 0]  # Probabilities of getting 0 in each basis
-
-    # Auxiliary vectors
-    V1 = p_Z - ((np.sin(theta / 2)) ** 2)
-    V2 = p_X - (1 / 2) * (np.sin(theta) + 1)
-
-    A = np.cos(theta) * V1 - np.sin(theta) * V2
-    B = np.sin(theta) * V1 + np.cos(theta) * V2
-    C = (1 / 2) - p_Y
-
-    return np.array([A, B, C])
-
-
-def gradient_step(hamiltonian:Hamiltonian, orig_angles, lr=0.05):
-    #TODO: Это хуевый градиент. Там должно еще домнажаться на коэффициенты, браться разница с целевым и пр пр
-    """Perform one step of gradient descent"""
-
-    # Coefficients for derivatives
-    A, B, C = get_ABC(hamiltonian, orig_angles)
-    coefs = np.array([A, B, C])
-
-    # Derivatives of angles
-    d_angles = single_gradient(coefs, orig_angles)
-
-    # One step of gradient descent with fixed learning rate lr
-    new_anlges = orig_angles - lr * d_angles
-
-    return new_anlges
+        return self.d_prob
 
 
-def gradient_descent(hamiltonian: Hamiltonian, orig_angles, lr=0.05, num_iterations=10):
-    angles = orig_angles
+class Gradient:
+    def __init__(self, hamiltonian_t: Hamiltonian, hamiltonian_g, original_angles):
+        self.hamiltonian_t = hamiltonian_t
+        self.hamiltonian_g = hamiltonian_g
+        self.original_angles = original_angles
 
-    # Make num_iterations of gradient descent steps
-    for i in range(num_iterations):
-        angles = gradient_step(hamiltonian, angles, lr)
+        self.prob_der_t = ProbabilityDerivative(hamiltonian_t, original_angles)
+        self.prob_der_g = ProbabilityDerivative(hamiltonian_g, original_angles)
+        self.d_prob_t = self.prob_der_t.d_prob()
+        self.d_prob_g = self.prob_der_g.d_prob()
 
-    return angles
+    def gradient(self):
+        """calculates gradient vector"""
+        d_angles = 2 * (self.prob_der_g.singles_Z - self.prob_der_t.singles_Z) * (self.d_prob_g - self.d_prob_t)
+        self.d_angles = d_angles
 
-def foo(x):
-    return x ** 3
+        return d_angles
+
+    def gradient_descent(self, lr=0.01, num_iterations=10):
+        """Perform gradient descent"""
+
+        for i in range(num_iterations):
+            self.original_angles += lr * self.gradient()
+
+        return self.original_angles
+
+
+
 # n_spins = 5
 # x = np.random.rand(n_spins) * 2 - 1
 # y = np.random.rand(n_spins) * 2 - 1
@@ -162,4 +176,3 @@ def foo(x):
 # A, B, C = get_ABC(hamiltonian, orig_angles)
 #
 # print(gradient_descent(hamiltonian, orig_angles))
-

modules/hamiltonian.py
@@ -46,11 +46,11 @@ class Hamiltonian:
 
                 static.append([field, fields_list])
 
-        for coupling in ['xx', 'yy', 'zz']:
-            if coupling in self.__dict__.keys():
-                couplings_list = self.__dict__[coupling]
-                couplings_list = [[couplings_list[i], i, (i + 1) % self.n_spins] for i in range(self.n_spins - 1)]
-                static.append([coupling, couplings_list])
+        # for coupling in ['xx', 'yy', 'zz']:
+        #     if coupling in self.__dict__.keys():
+        #         couplings_list = self.__dict__[coupling]
+        #         couplings_list = [[couplings_list[i], i, (i + 1) % self.n_spins] for i in range(self.n_spins - 1)]
+        #         static.append([coupling, couplings_list])
 
         basis = spin_basis_1d(self.n_spins)
         dynamic = []
@@ -84,12 +84,24 @@ class Hamiltonian:
 
     def measure(self, angles):
         density_mat = self.rotation(angles)
-        singles, correlators = ReducedMatrixMeasurement.reduced_matrix_measurements(density_mat)
-        return singles, correlators
+        singles = ReducedMatrixMeasurement.reduced_matrix_measurements(density_mat)
+        return singles
+
+
+def hamiltonian_difference(hamiltonian_1: Hamiltonian, hamiltonian_2: Hamiltonian):
+
+    parameters = ['x', 'y', 'z']
+    mse = 0
+
+    for parameter in parameters:
+        if hasattr(hamiltonian_1, parameter):
+            mse += ((hamiltonian_1.__dict__[parameter] - hamiltonian_2.__dict__[parameter]) ** 2).mean(axis=None)
+
+    return mse
 
 
 def u_mat(theta, phi):
-    """ Generates U (rotation matrix) by given angles of Bloch sphere"""
+    """ Generates U (rotation matrix of size (2, 2)) by given angles of Bloch sphere"""
 
     u11 = np.cos(theta / 2)
     u12 = np.sin(theta / 2) * np.exp(- phi * 1j)
@@ -102,7 +114,7 @@ def u_mat(theta, phi):
 
 
 def angles_to_umat(angles):
-    """Calculates rotation matrices
+    """Calculates rotation matrix of the whole system
     angles: np.array, columns are different spins, rows are theta and phi
     """
     # First matrix
@@ -123,7 +135,9 @@ def rotate(rho: np.array, u_mat):
     try:
         return u_mat @ rho @ u_mat.T.conj()
     except ValueError:
-        print('Didnt rotate matrix')
+        print(rho.shape)
+        print(u_mat.shape)
+        raise Warning('Didnt rotate matrix')
         return rho
 
 

modules/main.py
@@ -1,53 +1,104 @@
 from random import seed
 
 from modules.smc import Particle, Cloud
-from modules.measurements import ReducedMatrixMeasurement
-from modules.gradient import *
+from modules.gradient import ProbabilityDerivative, Gradient
+from modules.hamiltonian import *
+from modules.measurements import *
 
-np.random.seed(42)
-seed(42)
-n_spins = 3
-beta = 0.3
+np.random.seed(43)
+seed(43)
 
-# Create random basis angles
-theta = np.random.rand(n_spins) * np.pi
-phi = np.random.rand(n_spins) * np.pi
-angles = np.array([theta, phi]).T
+def main_cycle(n_cycles, lr, gradient_iterations):
+    # Some initial parameters
+    n_spins = 1
+    beta = 0.3
+
+    # Create initial random basis angles
+    theta = np.random.rand(n_spins) * np.pi
+    phi = np.random.rand(n_spins) * 2 * np.pi
+    # theta = np.array([0.])
+    # phi = np.array([0.])
+
+    angles = np.array([theta, phi]).T
+
+    # Create target hamiltonian
+    # x_t = np.random.rand(n_spins) * 2 - 1
+    # z_t = np.random.rand(n_spins) * 2 - 1
+    z_t = [-1]
+    # x_t = [0]
+
+    hamiltonian_t = Hamiltonian(n_spins, beta, z=z_t)
+    hamiltonian_t.set_density_mat()
+    print('Target density matrix')
+    print(hamiltonian_t.density_mat)
+
+    # Create an initial pull of random hamiltonians
+    # (we call it "particle" according to Sequential Monte Carlo terminology)
+    n_particles = 10
+    g_cloud = Cloud(n_particles, n_spins, beta, fields=["z"])
+    for i in range(n_cycles):
+        # Measure target hamiltonian
+        singles_t = hamiltonian_t.measure(angles)
+
+        # Update weight according to particle's distance to target hamiltonian
+        g_cloud.list_weight_update(angles, singles_t)
+
+        # Make a recycling wheel to get rid of far particles
+        g_cloud.resampling_wheel()
+
+        # Weighted sum of all particles -- our current best result
+        hamiltonian_g = g_cloud.weighted_sum()
+
+        mse = hamiltonian_difference(hamiltonian_t, hamiltonian_g)
+        print(f"iteration {i}")
+        print(f"mse {mse}")
+        print(f"hamiltonian_g.z {hamiltonian_g.z}")
+        singles_g = hamiltonian_g.measure(angles)
+        print(f"distance {distance_by_measurements(singles_g, singles_t)}")
+        print(f"theta {angles[0, 0] % np.pi}")
+        print(f"phi {angles[0, 1] % (2 * np.pi)}")
+        print('')
+
+        # Make a gradient descent to determine new angles
+        grad = Gradient(hamiltonian_t, hamiltonian_g, angles)
+        angles = grad.gradient_descent(lr=lr, num_iterations=gradient_iterations)
+
+    # print(hamiltonian_g.z)
+    # print(hamiltonian_t.z)
+    # print(angles)
+    # print(hamiltonian_g.density_mat)
+    # for particle in g_cloud.particles_list:
+    #     print(particle.z)
+    #     print(particle.weight)
 
 
-# Create target hamiltonian
-x_t = np.random.rand(n_spins) * 2 - 1
-z_t = np.random.rand(n_spins) * 2 - 1
-
-target = Hamiltonian(n_spins, beta, x=x_t, z=z_t)
-target.set_density_mat()
-
-# And measure it
-singles_t, correlators_t = target.measure(angles)
-
-
-# Create a pull of random hamiltonians (call it particle according to Sequential Monte Carlo terminology)
-n_particles = 30
-g_cloud = Cloud(n_particles, n_spins, beta, fields=["x", "y"])
-# print(g_cloud.particles_list[3].x)
-
-# Update weight according to particle's distance to target hamiltonian
-g_cloud.list_weight_update(angles, singles_t, correlators_t)
-
-# Make a recycling wheel to get rid of far particles
-g_cloud.resampling_wheel()
-
-# weighted sum of all particles -- our current best result
-g = g_cloud.weighted_sum()
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main_cycle(n_cycles=10, lr=1, gradient_iterations=3000)
+    # n_spins = 1
+    # beta = 100
+    #
+    # z_t = [-1]
+    # z_g = [1]
+    # z_f = [0]
+    #
+    # hamiltonian_t = Hamiltonian(n_spins, beta, z=z_t)
+    # hamiltonian_t.set_density_mat()
+    #
+    # hamiltonian_g = Hamiltonian(n_spins, beta, z=z_g)
+    # hamiltonian_g.set_density_mat()
+    #
+    # hamiltonian_f = Hamiltonian(n_spins, beta, z=z_f)
+    # hamiltonian_f.set_density_mat()
+    #
+    # theta = np.array([0
+    #                   ])
+    # phi = np.array([0.])
+    #
+    # angles = np.array([theta, phi]).T
+    #
+    # singles_t = hamiltonian_t.measure(angles)
+    # singles_g = hamiltonian_g.measure(angles)
+    # singles_f = hamiltonian_f.measure(angles)
+    #
+    # print(distance_by_measurements(singles_t, singles_g))
+    # print(distance_by_measurements(singles_t, singles_f))

modules/measurements.py
@@ -69,15 +69,15 @@ class ReducedMatrixMeasurement:
         n_spins = int(math.log2(dim))
 
         singles = np.zeros((n_spins, 2))  # single spins
-        correlators = np.zeros((n_spins - 1, 4))  # correlators distribution
+        # correlators = np.zeros((n_spins - 1, 4))  # correlators distribution
         for i in range(n_spins):
             single_rho = ReducedMatrixMeasurement.reduced_matrix(density_mat, i, i)
             singles[i] = np.diag(single_rho).real
-            if i + 1 < n_spins:
-                correlator_rho = ReducedMatrixMeasurement.reduced_matrix(density_mat, i, i+1)
-                correlators[i] = np.diag(correlator_rho).real
+            # if i + 1 < n_spins:
+            #     correlator_rho = ReducedMatrixMeasurement.reduced_matrix(density_mat, i, i+1)
+            #     correlators[i] = np.diag(correlator_rho).real
 
-        return singles, correlators
+        return singles
 
 
 class SamplingMeasurement:
@@ -126,9 +126,11 @@ class SamplingMeasurement:
 
 
 ### This funciton is universal ###
-def distance_by_measurements(singles_1, singles_2, correlators_1, correlators_2):
+def distance_by_measurements(singles_1, singles_2):
     """Find the distance between 2 matrices according to singles and correlators measurements"""
-    return sum(sum((singles_1 - singles_2) ** 2)) + sum(sum((correlators_2 - correlators_1) ** 2))
+    p0_1 = singles_1[:, 0]
+    p0_2 = singles_2[:, 0]
+    return ((p0_1 - p0_2) ** 2).mean()
 
 
 # n = 3

modules/smc.py
@@ -2,6 +2,7 @@
 import numpy as np
 from modules.hamiltonian import Hamiltonian
 import modules.measurements as measurements
+import copy
 
 
 class Particle(Hamiltonian):
@@ -12,17 +13,23 @@ class Particle(Hamiltonian):
     def set_weight(self, weight):
         self.weight = weight
 
-    def weight_update(self, angles, singles_t, correlators_t):
+    def scale_weight(self, scalar):
+        # to be sure
+        self.set_weight(self.weight * scalar)
+
+    def weight_update(self, angles, singles_t):
         """Update weight of particle according to it's distance to target hamiltonian"""
-        singles_g, correlators_g = self.measure(angles)
-        distance = measurements.distance_by_measurements(singles_g, singles_t, correlators_g, correlators_t)
-        weight = np.exp(- distance ** 2 / 2) / np.sqrt(2 * np.pi)
+        sigma = 0.01
+        singles_g = self.measure(angles)
+        distance = measurements.distance_by_measurements(singles_g, singles_t)
+        weight = np.exp(- ((distance / sigma) ** 2) * (1 / 2)) / np.sqrt(2 * np.pi * sigma)
         self.set_weight(weight)
+
 
 class Cloud:
     """Essentially, pull of particles"""
 
-    def __init__(self, n_particles, n_spins, beta=0.3, fields=None, correlators=None):
+    def __init__(self, n_particles, n_spins, beta=0.3, fields=None, couplings=None):
         """Creates an initial list of 'particles' -- hamiltonians with random coefficients and equal weights"""
 
         self.particles_list = []
@@ -31,42 +38,49 @@ class Cloud:
         self.beta = beta
         if fields:
             self.fields = fields
-        if correlators:
-            self.correlators = correlators
+        self.weights_list = np.array([1 / n_particles for i in range(n_particles)])
+        self.total_weight = sum(self.weights_list)
+        # if couplings:
+        #     self.couplings = couplings
 
         weight = 1 / n_particles
-        self.total_weight = 1
         for i in range(self.n_particles):
             particle = Particle(weight=weight, n_spins=n_spins, beta=beta)
 
             # This line adds random coefficients to hamiltonian
             if hasattr(self, 'fields'):
-                particle.__dict__.update({field: np.random.rand(self.n_spins) * 2 - 1 for field in self.fields})
-            if hasattr(self, 'correlators'):
-                particle.__dict__.update({corr: np.random.rand(self.n_spins - 1) * 2 - 1 for corr in self.correlators})
+                # particle.__dict__.update({field: np.random.randint(2, size=self.n_spins) * 2 - 1 for field in self.fields})  # Either +1 or -1
+                particle.__dict__.update({field: np.random.rand(n_spins) * 2 - 1 for field in
+                                          self.fields})
+            # if hasattr(self, 'couplings'):
+            #     particle.__dict__.update({coupling: np.random.rand(self.n_spins - 1) * 2 - 1 for coupling in self.couplings})
 
             particle.set_density_mat()
             self.particles_list.append(particle)
 
+    def set_weights_list(self):
+        weights = []
+        for particle in self.particles_list:
+            weights.append(particle.weight)
+        self.weights_list = np.array(weights)
+        return self.weights_list
+
     def weight_normalization(self):
-        """Normalizes weight of all particles such that the sum = 1"""
-        for i in range(len(self.particles_list)):
-            self.particles_list[i].weight /= self.total_weight
+        """Normalizes weight of all particles such that the sum of all weights = 1"""
+        self.total_weight = sum(self.set_weights_list())
+        for i in range(self.n_particles):
+            self.particles_list[i].scale_weight(1 / self.total_weight)
+        self.set_weights_list()
 
-        self.total_weight = 1
-
-
-    def list_weight_update(self, angles, singles_t, correlators_t):
+    def list_weight_update(self, angles, singles_t, correlators_t=None):
         """Update weight of particle according to it's distance to target hamiltonian"""
         self.total_weight = 0
 
         for particle in self.particles_list:
-            particle.weight_update(angles, singles_t, correlators_t)
-            self.total_weight += particle.weight
+            particle.weight_update(angles, singles_t)
 
         # normalize weights
         self.weight_normalization()
-
 
     def resampling_wheel(self):
         # sry, not my function
@@ -79,17 +93,17 @@ class Cloud:
         new_particles_list = []
         index = np.random.randint(self.n_particles)
         beta = 0
-        total_weight = 0
 
         for _ in range(self.n_particles):
             beta += np.random.uniform(0, 2 * max_weight)
             while self.particles_list[index].weight < beta:
                 beta -= self.particles_list[index].weight
                 index = (index + 1) % self.n_particles
-            new_particles_list.append(self.particles_list[index])
-            total_weight += self.particles_list[index].weight
 
-        self.particles_list = new_particles_list
+            new_particle = copy.deepcopy(self.particles_list[index])  # It's required to create a copy. Probably not the best variant to use "deepcopy"
+            new_particles_list.append(new_particle)
+
+        self.particles_list = np.array(new_particles_list)
         self.weight_normalization()
 
     def weighted_sum(self):
@@ -104,13 +118,13 @@ class Cloud:
 
                 resulting_particle.__dict__.update({field: f})
 
-        if hasattr(self, 'correlators'):
-            for correlator in self.correlators:
-                c = np.zeros(self.n_spins - 1)
-                for particle in self.particles_list:
-                    c += particle.__dict__[correlator] * particle.weight
-
-                resulting_particle.__dict__.update({correlator: c})
+        # if hasattr(self, 'couplings'):
+        #     for coupling in self.couplings:
+        #         c = np.zeros(self.n_spins - 1)
+        #         for particle in self.particles_list:
+        #             c += particle.__dict__[coupling] * particle.weight
+        #
+        #         resulting_particle.__dict__.update({coupling: c})
 
         resulting_particle.set_density_mat()
         self.resulting_particle = resulting_particle