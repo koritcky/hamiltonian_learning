gradient bug fixes

modules/gradient.py
@@ -4,6 +4,7 @@ from modules.hamiltonian import Hamiltonian
 
 class ProbabilityDerivative:
     """Perform measurements and calculates derivatives of probabilites for hamiltonian"""
+
     def __init__(self, hamiltonian: Hamiltonian, original_angles):
         self.hamiltonian = hamiltonian
         self.original_angles = original_angles
@@ -28,11 +29,13 @@ class ProbabilityDerivative:
         ZY = np.copy(original_angles)
         for i in range(self.n_spins):
             if i % 2 == 0:
-                XZ[i, 0] += np.pi / 2
-                YZ[i] += np.pi / 2  # Both angles are increases by pi/2
+                XZ[i, 0] += np.pi / 2  # theta
+                YZ[i, 0] = np.pi / 2  # theta
+                YZ[i, 1] += np.pi / 2  # phi
             else:
-                ZX[i, 0] += np.pi / 2
-                ZY[i] += np.pi / 2  # Both angles are increases by pi/2
+                ZX[i, 0] += np.pi / 2  # theta
+                ZY[i, 0] = np.pi / 2  # theta
+                ZY[i, 1] += np.pi / 2  # phi
         return ZZ, XZ, YZ, ZX, ZY
 
     def measurements_for_gradient(self):
@@ -105,8 +108,8 @@ class ProbabilityDerivative:
         p_Z, p_X, p_Y = self.singles_Z[:, 0], self.singles_X[:, 0], self.singles_Y[:, 0]
 
         # Auxiliary vectors
-        V1 = p_Z - ((np.sin(theta / 2)) ** 2) - (1 / 2) * np.cos(theta)
-        V2 = p_X - (1 / 2) * (np.sin(theta) + 1) + (1 / 2) * np.sin(theta)
+        V1 = p_Z - (1 / 2)
+        V2 = p_X - (1 / 2)
 
         A = - np.cos(theta) * V1 + np.sin(theta) * V2
         B = np.sin(theta) * V1 + np.cos(theta) * V2
@@ -162,7 +165,8 @@ class Gradient:
         return self.original_angles
 
 
-
+def foo(x):
+    return x ** 5
 # n_spins = 5
 # x = np.random.rand(n_spins) * 2 - 1
 # y = np.random.rand(n_spins) * 2 - 1

modules/main.py
@@ -5,10 +5,11 @@ from modules.gradient import ProbabilityDerivative, Gradient
 from modules.hamiltonian import *
 from modules.measurements import *
 
-np.random.seed(43)
-seed(43)
+# np.random.seed(43)
+# seed(43)
 
-def main_cycle(n_cycles, lr, gradient_iterations):
+
+def main_cycle(n_cycles, n_particles, lr, gradient_iterations):
     # Some initial parameters
     n_spins = 1
     beta = 0.3
@@ -34,7 +35,6 @@ def main_cycle(n_cycles, lr, gradient_iterations):
 
     # Create an initial pull of random hamiltonians
     # (we call it "particle" according to Sequential Monte Carlo terminology)
-    n_particles = 10
     g_cloud = Cloud(n_particles, n_spins, beta, fields=["z"])
     for i in range(n_cycles):
         # Measure target hamiltonian
@@ -55,50 +55,14 @@ def main_cycle(n_cycles, lr, gradient_iterations):
         print(f"hamiltonian_g.z {hamiltonian_g.z}")
         singles_g = hamiltonian_g.measure(angles)
         print(f"distance {distance_by_measurements(singles_g, singles_t)}")
-        print(f"theta {angles[0, 0] % np.pi}")
-        print(f"phi {angles[0, 1] % (2 * np.pi)}")
+        print(f"theta {angles[0, 0] / np.pi}")
+        print(f"phi {angles[0, 1] / np.pi}")
         print('')
 
         # Make a gradient descent to determine new angles
         grad = Gradient(hamiltonian_t, hamiltonian_g, angles)
         angles = grad.gradient_descent(lr=lr, num_iterations=gradient_iterations)
 
-    # print(hamiltonian_g.z)
-    # print(hamiltonian_t.z)
-    # print(angles)
-    # print(hamiltonian_g.density_mat)
-    # for particle in g_cloud.particles_list:
-    #     print(particle.z)
-    #     print(particle.weight)
-
 
 if __name__ == '__main__':
-    main_cycle(n_cycles=10, lr=1, gradient_iterations=3000)
-    # n_spins = 1
-    # beta = 100
-    #
-    # z_t = [-1]
-    # z_g = [1]
-    # z_f = [0]
-    #
-    # hamiltonian_t = Hamiltonian(n_spins, beta, z=z_t)
-    # hamiltonian_t.set_density_mat()
-    #
-    # hamiltonian_g = Hamiltonian(n_spins, beta, z=z_g)
-    # hamiltonian_g.set_density_mat()
-    #
-    # hamiltonian_f = Hamiltonian(n_spins, beta, z=z_f)
-    # hamiltonian_f.set_density_mat()
-    #
-    # theta = np.array([0
-    #                   ])
-    # phi = np.array([0.])
-    #
-    # angles = np.array([theta, phi]).T
-    #
-    # singles_t = hamiltonian_t.measure(angles)
-    # singles_g = hamiltonian_g.measure(angles)
-    # singles_f = hamiltonian_f.measure(angles)
-    #
-    # print(distance_by_measurements(singles_t, singles_g))
-    # print(distance_by_measurements(singles_t, singles_f))
+    main_cycle(n_cycles=50, n_particles=500, lr=0.01, gradient_iterations=3000)