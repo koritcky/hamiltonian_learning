fixed gradient phi bug

modules/gradient.py
@@ -36,6 +36,7 @@ class ProbabilityDerivative:
                 ZX[i, 0] += np.pi / 2  # theta
                 ZY[i, 0] = np.pi / 2  # theta
                 ZY[i, 1] += np.pi / 2  # phi
+
         return ZZ, XZ, YZ, ZX, ZY
 
     def measurements_for_gradient(self):
@@ -130,7 +131,7 @@ class ProbabilityDerivative:
 
         # Actually, this is derivatives of probabilities by d_theta and d_phi
         d_theta = A * np.sin(theta) + B * np.cos(theta)  # \frac{d_prob}{d_theta}
-        d_phi = - C * np.sin(theta)  # \frac{d_prob}{d_phi}
+        d_phi = C * np.sin(theta)  # \frac{d_prob}{d_phi}
 
         d_prob = np.array([d_theta, d_phi]).T
         self.d_prob = d_prob
@@ -158,15 +159,12 @@ class Gradient:
 
     def gradient_descent(self, lr=0.01, num_iterations=10):
         """Perform gradient descent"""
-
         for i in range(num_iterations):
             self.original_angles += lr * self.gradient()
 
         return self.original_angles
 
 
-def foo(x):
-    return x ** 5
 # n_spins = 5
 # x = np.random.rand(n_spins) * 2 - 1
 # y = np.random.rand(n_spins) * 2 - 1

modules/main.py
@@ -11,7 +11,7 @@ from modules.measurements import *
 
 def main_cycle(n_cycles, n_particles, lr, gradient_iterations):
     # Some initial parameters
-    n_spins = 1
+    n_spins = 3
     beta = 0.3
 
     # Create initial random basis angles
@@ -23,10 +23,10 @@ def main_cycle(n_cycles, n_particles, lr, gradient_iterations):
     angles = np.array([theta, phi]).T
 
     # Create target hamiltonian
-    # x_t = np.random.rand(n_spins) * 2 - 1
-    # z_t = np.random.rand(n_spins) * 2 - 1
-    z_t = [-1]
-    # x_t = [0]
+    x_t = np.random.rand(n_spins) * 2 - 1
+    z_t = np.random.rand(n_spins) * 2 - 1
+    # z_t = [-1, 0.3, 0.8]
+    # # x_t = [0]
 
     hamiltonian_t = Hamiltonian(n_spins, beta, z=z_t)
     hamiltonian_t.set_density_mat()
@@ -62,7 +62,10 @@ def main_cycle(n_cycles, n_particles, lr, gradient_iterations):
         # Make a gradient descent to determine new angles
         grad = Gradient(hamiltonian_t, hamiltonian_g, angles)
         angles = grad.gradient_descent(lr=lr, num_iterations=gradient_iterations)
-
+    print(singles_t)
+    print(singles_g)
 
 if __name__ == '__main__':
-    main_cycle(n_cycles=50, n_particles=500, lr=0.01, gradient_iterations=3000)
+    main_cycle(n_cycles=300, n_particles=500, lr=0.1, gradient_iterations=300)
+
+

modules/smc.py
@@ -19,10 +19,11 @@ class Particle(Hamiltonian):
 
     def weight_update(self, angles, singles_t):
         """Update weight of particle according to it's distance to target hamiltonian"""
-        sigma = 0.01
+        sigma = 10 ** (-4)
         singles_g = self.measure(angles)
         distance = measurements.distance_by_measurements(singles_g, singles_t)
         weight = np.exp(- ((distance / sigma) ** 2) * (1 / 2)) / np.sqrt(2 * np.pi * sigma)
+        # weight = 1/distance
         self.set_weight(weight)
 
 