1ebef907414dad65bde4df75589040dc67323a33
koritcky <koritcky@gmail.com>
2020-04-30 14:27:50 +0300

    modernize resampling

--- main.py
+++ main.py
@@ -29,30 +29,33 @@ def main_cycle(n_cycles, n_particles, lr, gradient_iterations):
     # x_t = np.random.rand(n_spins) * 2 - 1
     z_t = np.random.rand(n_spins) * 2 - 1
     x_t = np.random.rand(n_spins) * 2 - 1
-    # zz_t = np.random.rand(n_spins - 1) * 2 - 1
+    zz_t = np.random.rand(n_spins - 1) * 2 - 1
 
-    hamiltonian_t = Hamiltonian(n_spins, beta, z=z_t, x=x_t)
+    hamiltonian_t = Hamiltonian(n_spins, beta, z=z_t, x=x_t, zz=zz_t)
     hamiltonian_t.set_density_mat()
     # print('Target density matrix')
     # print(hamiltonian_t.density_mat)
 
     # Create an initial pull of random hamiltonians
     # (we call it "particle" according to Sequential Monte Carlo terminology)
-    g_cloud = Cloud(n_particles, n_spins, beta, fields=["z", 'x'])
+    g_cloud = Cloud(n_particles, n_spins, beta, coeffs_types=["z", 'x', 'zz'])
     MSE = []
 
     for i in range(n_cycles):
         # Measure target hamiltonian
         singles_t, correlators_t = hamiltonian_t.measure(angles)
+
         # Update weight according to particle's distance to target hamiltonian
         g_cloud.list_weight_update(angles, singles_t, correlators_t)
 
         # Make a recycling wheel to get rid of far particles
-        g_cloud.resampling_wheel()
+        g_cloud.resampling()
 
         # Weighted sum of all particles -- our current best result
-        hamiltonian_g = g_cloud.weighted_sum()
+        # hamiltonian_g = g_cloud.weighted_sum()
 
+        X = g_cloud.get_X()
+        hamiltonian_g, _ = g_cloud.mean_vec(X)
         mse = hamiltonian_difference(hamiltonian_t, hamiltonian_g)
         MSE.append(mse)
         # print(f"iteration {i}")
@@ -67,14 +70,14 @@ def main_cycle(n_cycles, n_particles, lr, gradient_iterations):
         # Make a gradient descent to determine new angles
         grad = Gradient(hamiltonian_t, hamiltonian_g, angles)
         angles = grad.gradient_descent(lr=lr, num_iterations=gradient_iterations)
-    print(f"z_t = {z_t}, zz_t = {x_t}")
-    print(f"z_g = {hamiltonian_g.z}, zz_g = {hamiltonian_g.x}")
+    print(f"z_t = {z_t}, x_t = {x_t}")
+    print(f"z_g = {hamiltonian_g.z}, x_g = {hamiltonian_g.x}")
     return MSE, g_cloud
 
 
 if __name__ == '__main__':
     n_cycles = 20
-    MSE, g_cloud = main_cycle(n_cycles=n_cycles, n_particles=300, lr=0.3, gradient_iterations=150)
+    MSE, g_cloud = main_cycle(n_cycles=n_cycles, n_particles=500, lr=0.5, gradient_iterations=500)
     X = np.linspace(1, n_cycles, n_cycles)
     plt.plot(X, MSE)
     plt.show()
--- modules/smc.py
+++ modules/smc.py
@@ -13,25 +13,49 @@ class Particle(Hamiltonian):
     def set_weight(self, weight):
         self.weight = weight
 
-    def scale_weight(self, scalar):
-        # to be sure
-        self.set_weight(self.weight * scalar)
+    # def weight_update(self, mean, sd):
+    #     """Update weight of particle according to it's distance to target hamiltonian
+    #     mean is distance
+    #     sd is its standard deviation
+    #     """
+    #     # TODO this parameter must be adjusted and justified
+    #     weight = np.exp(- ((mean / sd) ** 2) * (1 / 2)) / np.sqrt(2 * np.pi * sd)
+    #     # weight = 1/distance
+    #     self.set_weight(weight)
 
-    def weight_update(self, mean, sd):
-        """Update weight of particle according to it's distance to target hamiltonian
-        mean is distance
-        sd is its standard deviation
-        """
-         # ?? this parameter must be adjusted and justified
-        weight = np.exp(- ((mean / sd) ** 2) * (1 / 2)) / np.sqrt(2 * np.pi * sd)
-        # weight = 1/distance
-        self.set_weight(weight)
+    @staticmethod
+    def vectorize(dic: dict):
+        vec = []
+        for field in ['x', 'y', 'z']:
+            if field in dic.keys():
+                vec += list(dic[field])
+
+        for coupling in ['xx', 'yy', 'zz']:
+            if coupling in dic.keys():
+                vec += list(dic[coupling])
+        return vec
+
+    @staticmethod
+    def dictorize(v: list, n_spins, coeffs_types):
+        dic = {}
+        i = 0
+
+        for field in ['x', 'y', 'z']:
+            if field in coeffs_types:
+                dic[field] = v[i: i + n_spins]
+                i += n_spins
+        for coupling in ['xx', 'yy', 'zz']:
+            if coupling in coeffs_types:
+                dic[coupling] = v[i: i + n_spins - 1]
+                i += n_spins - 1
+
+        return dic
 
 
 class Cloud:
     """Essentially, pull of particles"""
 
-    def __init__(self, n_particles, n_spins, beta=0.3, fields=None, couplings=None):
+    def __init__(self, n_particles, n_spins, beta, coeffs_types):
         """Creates an initial list of 'particles' -- hamiltonians with random coefficients and equal weights"""
 
         self.particles_list = []
@@ -39,26 +63,23 @@ class Cloud:
         self.n_spins = n_spins
         self.beta = beta
 
-        if fields:
-            self.fields = fields
-        if couplings:
-            self.couplings = couplings
+        self.coeffs_types = coeffs_types
+        self.coeffs_len = n_spins * len(coeffs_types)
 
-        self.weights_list = np.array([1 / n_particles for i in range(n_particles)])
+        for type in coeffs_types:
+            if len(type) == 2:
+                self.coeffs_len -= 1
+
+        self.weights_list = np.array([1 / n_particles for _ in range(n_particles)])
         self.total_weight = sum(self.weights_list)
 
         weight = 1 / n_particles
         for i in range(self.n_particles):
-            particle = Particle(weight=weight, n_spins=n_spins, beta=beta)
 
-            # This line adds random coefficients to hamiltonian
-            if hasattr(self, 'fields'):
-                # particle.__dict__.update({field: np.random.randint(2, size=self.n_spins) * 2 - 1 for field in self.fields})  # Either +1 or -1
-                particle.__dict__.update({field: np.random.rand(n_spins) * 2 - 1 for field in
-                                          self.fields})
-            if hasattr(self, 'couplings'):
-                particle.__dict__.update({coupling: np.random.rand(self.n_spins - 1) * 2 - 1 for coupling in self.couplings})
+            rand_coeff = np.random.rand(self.coeffs_len) * 2 - 1
+            rand_coeff = Particle.dictorize(rand_coeff, self.n_spins, self.coeffs_types)
 
+            particle = Particle(weight=weight, n_spins=n_spins, beta=beta, **rand_coeff)
             particle.set_density_mat()
             self.particles_list.append(particle)
 
@@ -73,7 +94,7 @@ class Cloud:
         """Normalizes weight of all particles such that the sum of all weights = 1"""
         self.total_weight = sum(self.set_weights_list())
         for i in range(self.n_particles):
-            self.particles_list[i].scale_weight(1 / self.total_weight)
+            self.particles_list[i].weight /= self.total_weight
         self.set_weights_list()
 
     def list_weight_update(self, angles, singles_t, correlators_t=None):
@@ -87,66 +108,51 @@ class Cloud:
             mean.append(measurements.distance_by_measurements(singles_g, singles_t, correlators_g, correlators_t))
         sd = np.sqrt(1 / (self.n_particles - 1) * np.sum(np.array(mean) ** 2))
 
-        for i in range(len(self.particles_list)):
+        for i in range(self.n_particles):
             self.particles_list[i].weight = np.exp(- ((mean[i] / sd) ** 2) * (1 / 2)) / np.sqrt(2 * np.pi * sd)
 
         # normalize weights
         self.weight_normalization()
 
-    def resampling_wheel(self):
-        # sry, not my function
+    def resampling(self, alpha=0.98):
+        """Process of removing far away particles and adding gauss noise"""
+        X = self.get_X()
+        _, mean = self.mean_vec(X)
+        cov = self.cov(X) * (1 - alpha ** 2)
 
-        max_weight = 0
-        for particle in self.particles_list:
-            if particle.weight > max_weight:
-                max_weight = particle.weight
-
+        new_particles_ind = np.random.choice(self.n_particles, self.n_particles, replace=True, p=self.weights_list)
         new_particles_list = []
-        index = np.random.randint(self.n_particles)
-        beta = 0
-
-        for _ in range(self.n_particles):
-            beta += np.random.uniform(0, 2 * max_weight)
-            while self.particles_list[index].weight < beta:
-                beta -= self.particles_list[index].weight
-                index = (index + 1) % self.n_particles
-
-            new_particle = copy.deepcopy(self.particles_list[index])  # It's required to create a copy. Probably not the best variant to use "deepcopy"
-            new_particles_list.append(new_particle)
+        weight = 1 / self.n_particles
+        for i in new_particles_ind:
+            mean_i = alpha * X[i] + (1 - alpha) * mean
+            x = np.random.multivariate_normal(mean_i,  cov)
+            coefs = Particle.dictorize(x, self.n_spins, coeffs_types=self.coeffs_types)
+            particle = Particle(weight, self.n_spins, self.beta, **coefs)
+            particle.set_density_mat()
+            new_particles_list.append(particle)
 
         self.particles_list = np.array(new_particles_list)
         self.weight_normalization()
 
-    def weighted_sum(self):
+    def get_X(self):
+        X = []
+        for particle in self.particles_list:
+            x = Particle.vectorize({coeff: particle.__dict__[coeff] for coeff in self.coeffs_types})
+            X.append(x)
+        return np.array(X)
+
+
+    def mean_vec(self, X):
         """weighted sum of all particles"""
-        resulting_particle = Hamiltonian(self.n_spins, self.beta)
 
-        if hasattr(self, 'fields'):
-            for field in self.fields:
-                f = np.zeros(self.n_spins)
-                for particle in self.particles_list:
-                    f += particle.__dict__[field] * particle.weight
+        mean_vec = self.weights_list @ X
+        mean_dic = Particle.dictorize(mean_vec, self.n_spins, self.coeffs_types)
+        mean_particle = Hamiltonian(self.n_spins, self.beta, **mean_dic)
+        mean_particle.set_density_mat()
 
-                resulting_particle.__dict__.update({field: f})
+        return mean_particle, mean_vec
 
-        if hasattr(self, 'couplings'):
-            for coupling in self.couplings:
-                c = np.zeros(self.n_spins - 1)
-                for particle in self.particles_list:
-                    c += particle.__dict__[coupling] * particle.weight
-
-                resulting_particle.__dict__.update({coupling: c})
-
-        resulting_particle.set_density_mat()
-        self.resulting_particle = resulting_particle
-
-        return self.resulting_particle
-
-
-
-
-
-# particles_list = Particle.initial_generation(n_particles=5, n_spins=3, beta=0.3, fields=['x', 'z'])
-# print(len(particles_list))
-# print(np.trace(particles_list[0].density_mat@particles_list[0].density_mat))
+    def cov(self, X):
+        _, mean_vec = self.mean_vec(X)
+        return np.einsum('k, ki, kj-> ij', self.weights_list, X, X) - np.outer(mean_vec, mean_vec)
 
